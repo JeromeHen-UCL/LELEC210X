6b1d0ac33b33eebe5470daaf295c76c543450eb5
Jérôme Hendrick <jerome.hendrick@student.uclouvain.be>
2024-11-29 17:10:52 +0100

    uart-reader.py done

--- mcu/hands_on_feature_vectors/uart-reader.py
+++ mcu/hands_on_feature_vectors/uart-reader.py
@@ -3,7 +3,13 @@ uart-reader.py
 ELEC PROJECT - 210x
 """
 
+import sys
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.base import BaseEstimator, ClassifierMixin
+import pickle  # for loading the model
 import argparse
+import sys
+
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -23,9 +29,9 @@ dt = np.dtype(np.uint16).newbyteorder("<")
 def parse_buffer(line):
     line = line.strip()
     if line.startswith(PRINT_PREFIX):
-        return bytes.fromhex(line[len(PRINT_PREFIX) :])
+        return bytes.fromhex(line[len(PRINT_PREFIX):])
     else:
-        print(line)
+        # print(line)
         return None
 
 
@@ -37,18 +43,19 @@ def reader(port=None):
             line += ser.read_until(b"\n", size=2 * N_MELVECS * MELVEC_LENGTH).decode(
                 "ascii"
             )
-            print(line)
+            # print(line)
         line = line.strip()
         buffer = parse_buffer(line)
         if buffer is not None:
             buffer_array = np.frombuffer(buffer, dtype=dt)
 
-            yield buffer_array
+            yield buffer_array / np.linalg.norm(buffer_array)
 
 
-if __name__ == "__main__":
+def main():
     argParser = argparse.ArgumentParser()
     argParser.add_argument("-p", "--port", help="Port for serial communication")
+    argParser.add_argument("-m", "--model", help="Classification pickle filename")
     args = argParser.parse_args()
     print("uart-reader launched...\n")
 
@@ -62,24 +69,41 @@ if __name__ == "__main__":
             print(p.device)
         print("================")
         print("Launch this script with [-p PORT_REF] to access the communication port")
+        sys.exit(0)
 
-    else:
-        input_stream = reader(port=args.port)
-        msg_counter = 0
+    if args.model:
+        # load the trained model
+        with open(args.model, 'rb') as model_file:
+            classifier: ClassifierMixin | GaussianProcessClassifier = pickle.load(model_file)
 
-        for melvec in input_stream:
-            msg_counter += 1
+    input_stream = reader(port=args.port)
 
-            print(f"MEL Spectrogram #{msg_counter}")
+    for i, melvec in enumerate(input_stream):
 
-            plt.figure()
-            plot_specgram(
-                melvec.reshape((N_MELVECS, MELVEC_LENGTH)).T,
-                ax=plt.gca(),
-                is_mel=True,
-                title=f"MEL Spectrogram #{msg_counter}",
-                xlabel="Mel vector",
-            )
-            plt.draw()
-            plt.pause(0.001)
-            plt.clf()
+        # melvec /= np.linalg.norm(melvec, keepdims=True)
+
+        print(f"MEL Spectrogram #{i}")
+
+        plt.figure()
+        plot_specgram(
+            melvec.reshape((N_MELVECS, MELVEC_LENGTH)).T,
+            ax=plt.gca(),
+            is_mel=True,
+            title=f"MEL Spectrogram #{i}",
+            xlabel="Mel vector",
+        )
+        plt.draw()
+        plt.pause(0.001)
+        plt.clf()
+
+        if args.model:
+            # print(f"{melvec.shape=}")
+            prediction = classifier.predict((melvec,))
+            prediction_proba = classifier.predict_proba((melvec,))
+            print(f"{str(prediction[0])} : {prediction_proba[0]}")
+
+        # print(repr(melvec))
+
+
+if __name__ == "__main__":
+    main()
