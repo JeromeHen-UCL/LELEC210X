ac69c9716248f5a64c08760e04cd1fd3a8d5047d
Jérôme Hendrick <jerome.hendrick@student.uclouvain.be>
2025-02-18 15:47:31 +0100

    update classify

--- classification/src/classification/__main__.py
+++ classification/src/classification/__main__.py
@@ -1,17 +1,22 @@
-import pickle
+import os
 from pathlib import Path
+import pickle
 from typing import Optional
 
 import click
+import requests
 
-import common
 from auth import PRINT_PREFIX
+import common
 from common.env import load_dotenv
 from common.logging import logger
 
 from .utils import payload_to_melvecs
 
 load_dotenv()
+
+HOSTNAME_LOCAL = "http://localhost:5000"
+HOSTNAME_CONTEST = "http://lelec210x.sipr.ucl.ac.be/lelec210x"
 
 
 @click.command()
@@ -26,16 +31,25 @@ load_dotenv()
 @click.option(
     "-m",
     "--model",
+    "model_path",
     default=None,
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
     help="Path to the trained classification model.",
+)
+@click.option(
+    "--host",
+    "host",
+    default=None,
+    type=click.Choice(["local", "contest"]),
+    help="Choice of host for the contest. If none specified there will be no server submission.",
 )
 @common.click.melvec_length
 @common.click.n_melvecs
 @common.click.verbosity
 def main(
     _input: Optional[click.File],
-    model: Optional[Path],
+    model_path: Optional[Path],
+    host: Optional[str],
     melvec_length: int,
     n_melvecs: int,
 ) -> None:
@@ -51,19 +65,45 @@ def main(
     This way, you will directly receive the authentified packets from STDIN
     (standard input, i.e., the terminal).
     """
-    if model:
-        with open(model, "rb") as file:
-            m = pickle.load(file)
-    else:
-        m = None
 
-    for payload in _input:
-        if PRINT_PREFIX in payload:
-            payload = payload[len(PRINT_PREFIX) :]
+    logger.info("Starting classification...")
+
+    if model_path:
+        with open(model_path, "rb") as file:
+            model = pickle.load(file)
+    else:
+        model = None
+
+    # Why python 3.9? could have used match case :'(
+    # get the server address
+    # get the server key in order to send guesses
+    if host == "local":
+        hostname = HOSTNAME_LOCAL
+        key = os.getenv("LOCAL_KEY")
+    elif host == "contest":
+        hostname = HOSTNAME_CONTEST
+        key = os.getenv("CONTEST_KEY")
+    elif host is None:
+        hostname = None
+        key = None
+    else:
+        logger.error("No correct host chosen, got %s", host)
+        hostname = None
+        key = None
+
+    for packet in _input:
+        if PRINT_PREFIX in packet:
+            payload = packet[len(PRINT_PREFIX):]
 
             melvecs = payload_to_melvecs(payload, melvec_length, n_melvecs)
-            logger.info(f"Parsed payload into Mel vectors: {melvecs}")
+            logger.info("Parsed payload into Mel vectors: %s", melvecs)
 
-            if m:
-                # TODO: perform classification
-                pass
+            if model:
+                predictions = model.predict(melvecs)
+                prediction_proba = model.predict_proba(melvecs)
+                logger.debug("%s : %s ", str(predictions[0]), prediction_proba[0])
+
+                if hostname:
+                    logger.debug("Submitting prediction %s to %s", predictions[0], hostname)
+                    requests.post(f"{hostname}/lelec210x/leaderboard/submit/\
+                                {key}/{predictions}", timeout=1)
